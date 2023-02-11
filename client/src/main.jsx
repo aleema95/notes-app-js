9113b00452394f4cb9d2e2c6584f84705e2ce93c
aleema95 <alejandro.manouellian@gmail.com>
2023-02-11 13:30:05 -0300

    add react-router-dom library

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.scss'
+import Register from './components/Register/Register';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path: "/register",
+    element: <Register />
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
