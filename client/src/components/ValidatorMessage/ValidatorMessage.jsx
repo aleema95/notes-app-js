e525c2d1334bba6fda3504eb54d8abe3f7fa3958
aleema95 <alejandro.manouellian@gmail.com>
2023-02-15 06:23:59 -0300

    add basic validators with no css

--- client/src/components/ValidatorMessage/ValidatorMessage.jsx
+++ client/src/components/ValidatorMessage/ValidatorMessage.jsx
@@ -10,9 +10,28 @@ export default function ValidatorMessage() {
   return (
     <div className={s.main_container}>
       <div>
-        <h4>Name:</h4>
+        <h4>Username:</h4>
         <p className={`${formErrors.name ? null : s.validField}`}>*This field is required</p>
-        <p>*Only letters</p>
+      </div>
+      <div>
+        <h4>Name:</h4>
+        {/* create invalid field class, with red text */}
+        <p className={`${formErrors.name ? null : s.validField}`}>*This field is required</p>
+        <p className={`${formErrors.name_only_Letters ? null : s.validField}`}>*Only letters</p>
+      </div>
+      <div>
+        <h4>Last Name:</h4>
+        <p className={`${formErrors.last_name ? null : s.validField}`}>*This field is required</p>
+        <p className={`${formErrors.last_name_only_Letters ? null : s.validField}`}>*Only letters</p>
+      </div>
+      <div>
+        <h4>Email:</h4>
+        <p className={`${formErrors.email ? null : s.validField}`}>*This field is required</p>
+        <p className={`${formErrors.invalid_email ? null : s.validField}`}>*Email is invalid</p>
+      </div>
+      <div>
+        <h4>Password:</h4>
+        <p className={`${formErrors.password ? null : s.validField}`}>*This field is required</p>
       </div>
     </div>
   )
--- client/src/utils/registerUtils.js
+++ client/src/utils/registerUtils.js
@@ -5,14 +5,14 @@ export function validation(input) {
   const emailValidator = /[A-Za-z0-9.-]+@[a-zA-Z-]+\.(com|edu|net)$/g;
 
   if(!input.name) {
-    errors.nameOnlyLetters = true;
+    errors.name_only_Letters = true;
     errors.name = true;
-  } else if (onlyLetters.test(input.name)) errors.nameOnlyLetters = true;
+  } else if (onlyLetters.test(input.name)) errors.name_only_Letters = true;
   
   if(!input.last_name) {
     errors.last_name = true;
-    errors.last_nameOnlyLetters = true;
-  } else if (onlyLetters.test(input.last_name)) errors.last_nameOnlyLetters = true;
+    errors.last_name_only_Letters = true;
+  } else if (onlyLetters.test(input.last_name)) errors.last_name_only_Letters = true;
 
   if(!input.email) {
     errors.email = true;
