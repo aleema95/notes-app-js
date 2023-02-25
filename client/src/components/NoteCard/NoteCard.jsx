5149158c7ff6cabe188fe324c449660c2c34fdee
aleema95 <alejandro.manouellian@gmail.com>
2023-02-25 14:06:27 -0300

    add drag carousel to user profile

--- client/src/components/NoteCard/NoteCard.jsx
+++ client/src/components/NoteCard/NoteCard.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import s from './NoteCard.module.scss'
+import { motion } from 'framer-motion'
+import { Link } from 'react-router-dom'
 
 export default function NoteCard({id, title, body, rating, UserId }) {
   return (
-      <div className={s.card_container}>
+      <motion.div className={s.card_container}>
         <div className={s.note_header}>
           <h4>{title}</h4>
           <h4>{rating}/5</h4>
@@ -14,6 +16,6 @@ export default function NoteCard({id, title, body, rating, UserId }) {
         <h4 className={s.note_author}>
           - Seneca
         </h4>
-      </div>
+      </motion.div>
   )
 }
