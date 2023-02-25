c7d624a02a98a3cb3046e29a0ec4ddc545448882
aleema95 <alejandro.manouellian@gmail.com>
2023-02-25 18:41:28 -0300

    add CardCarousel component

---
+++ client/src/components/CardCarousel/CardCarousel.jsx
@@ -0,0 +1,27 @@
+import React, { useState, useRef, useEffect} from 'react'
+import { motion } from 'framer-motion';
+import NoteCard from '../NoteCard/NoteCard';
+import s from './CardCarousel.module.scss'
+
+export default function CardCarousel({ notes }) {
+  const [width, setWidth] = useState(0)
+  const carousel = useRef();
+
+  useEffect(() => {
+    setTimeout(() => {
+      setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth)
+    }, 50)
+  }, [])
+
+  return (
+      <motion.div ref={carousel} className={s.user_notes_carousel}>
+        <motion.div drag='x' dragConstraints={{right:0, left: -width}} className={s.inner_carousel}>
+          {
+            notes?.map( (n, i) => {
+              return <NoteCard {...n} key={i} />
+            })
+          }
+        </motion.div>
+      </motion.div>
+  )
+}
