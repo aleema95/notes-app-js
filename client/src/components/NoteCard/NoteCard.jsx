2adb0e42e5dec0917f59753e5957f8925de1f773
aleema95 <alejandro.manouellian@gmail.com>
2023-02-27 06:25:17 -0300

    add svg favourite icon

--- client/src/components/NoteCard/NoteCard.jsx
+++ client/src/components/NoteCard/NoteCard.jsx
@@ -2,20 +2,26 @@ import React from 'react'
 import s from './NoteCard.module.scss'
 import { motion } from 'framer-motion'
 import { Link } from 'react-router-dom'
+import FavouriteIcon from '../../assets/svg/FavouriteIcon'
 
 export default function NoteCard({id, title, body, rating, UserId }) {
   return (
       <motion.div className={s.card_container}>
-        <div className={s.note_header}>
-          <h4>{title}</h4>
-          <h4>{rating}/5</h4>
-        </div>
+        <Link to={'/'} className={s.title_link}>
+          <div className={s.note_header}>
+            <h4>{title}</h4>
+            <h4>{rating}/5</h4>
+          </div>
+        </Link>  
         <p className={s.note_body}>
           {body}
         </p>
-        <h4 className={s.note_author}>
-          - Seneca
-        </h4>
+        <div className={s.bottom_container}>
+          <FavouriteIcon />
+          <h4 className={s.note_author}>
+            - Seneca
+          </h4>
+        </div>
       </motion.div>
   )
 }
