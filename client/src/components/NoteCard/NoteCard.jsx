80d0ee07d4ad171a1586d9ffa954aa68e0ee6a9d
aleema95 <alejandro.manouellian@gmail.com>
2023-03-01 06:16:21 -0300

    add changes to user-profile-component

--- client/src/components/NoteCard/NoteCard.jsx
+++ client/src/components/NoteCard/NoteCard.jsx
@@ -6,11 +6,20 @@ import FavouriteIcon from '../../assets/svg/FavouriteIcon'
 import FavouriteIconChecked from '../../assets/svg/FavouriteIconChecked'
 
 export default function NoteCard({id, title, body, rating, UserId }) {
-  const [favourite, setfavourite] = useState(false)
+  const [favourite, setFavourite] = useState(false)
+
+  function onFavClick(e) {
+    setFavourite(!favourite)
+  }
+
+  // function onTitleClick(e) {
+  //   e.preventDefault()
+  // }
+
   return (
-      <motion.div className={s.card_container}>
-        <Link to={'/'} className={s.title_link}>
-          <div className={s.note_header}>
+      <motion.div  className={s.card_container}>
+        <Link to={'/'} className={s.title_link} >
+          < div className={s.note_header}>
             <h4>{title}</h4>
             <h4>{rating}/5</h4>
           </div>
@@ -19,7 +28,9 @@ export default function NoteCard({id, title, body, rating, UserId }) {
           {body}
         </p>
         <div className={s.bottom_container}>
-          {favourite ? <FavouriteIconChecked /> : <FavouriteIcon />}
+          <div onClick={onFavClick}>
+            {favourite ? <FavouriteIconChecked /> : <FavouriteIcon />}
+          </div>
           <h4 className={s.note_author}>
             - Seneca
           </h4>
