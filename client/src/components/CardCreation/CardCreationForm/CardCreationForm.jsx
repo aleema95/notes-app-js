49424f3f2645364eeff32a47c747ef9825d4e941
aleema95 <alejandro.manouellian@gmail.com>
2023-03-12 06:08:11 -0300

    add functionality to cardCreation component

--- client/src/components/CardCreation/CardCreationForm/CardCreationForm.jsx
+++ client/src/components/CardCreation/CardCreationForm/CardCreationForm.jsx
@@ -1,17 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import s from './CardCreationForm.module.scss'
 
 export default function CardCreationForm() {
+  const [noteData, setNoteData] = useState({title: '', body: ''})
+
+  function onChange(e){
+    setNoteData({...noteData, [e.target.name]: e.target.value})
+  }
+
+  function onSubmit(e){
+    e.preventDefault()
+    if (!noteData.title || !noteData.body) return
+    //Create sendForm function
+    alert('submitted')
+  }
+
   return (
-    <form className={s.form_container} action="">
+    <form onSubmit={onSubmit} className={s.form_container} action="">
       <div className={s.title_input_container}>
         <label htmlFor="">Title</label>
-        <input name="title" type="text" />
+        <input onChange={onChange} name="title" type="text" />
       </div>
       <div className={s.body_input_container}>
         <label  htmlFor="">Body</label>
-        <textarea name="body"  cols="19" rows="4"  class="text" />
-        {/* <input name="body" type="text-area" /> */}
+        <textarea onChange={onChange} name="body"  cols="19" rows="4"  class="text" />
       </div>
       <button className={s.submit_btn} type='submit'>Create Note</button>
     </form>
